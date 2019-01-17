1a114b1f879beaf13f4dcd54e703824c0feea6f6
Naman16Bhatt <naman.bhatt@iiitb.org>
2019-01-17 10:31:10 +0530

    master commit

---
+++ Branch.java
@@ -0,0 +1,16 @@
+import java.io.File;
+import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.io.*; 
+class Branch{
+    public static void main(String ar[]){
+     //   File file=new File("/home/naman/java_jobs/input3.txt");
+       // Scanner sc = new Scanner(file);
+        //   int i = 0;
+      int  a=5, b=6;
+        System.out.println("Sum:" + (a + b));
+        
+    }
+  
+}
